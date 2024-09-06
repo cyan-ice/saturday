9d045c393441958bd679c57f720cdeea36d08677
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2024-09-06 11:32:37 +0000

    Update saturday.py

--- saturday.py
+++ saturday.py
@@ -1,5 +1,7 @@
 __version__ = '0.0.1'
 
+import os
+
 from argparse import ArgumentParser, FileType
 from sys import stderr, stdin
 from operator import mul, add, sub, mod, and_, or_, xor, not_, inv
@@ -99,98 +101,107 @@ def if_skip():
     i = cor[left[i]]
 
 
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 if args.debug:
-    print(st)
-while i < len(code):
-    ch = code[i]
-    if args.debug and not ch.isspace():
-        print(ch)
-    match ch:
-        case '!':
-            st.append(operation(lambda x: int(not_(x)), *stack_pop()))
-        case '%':
-            st.append(operation(mod, *stack_pop(2)))
-        case '&':
-            st.append(operation(and_, *stack_pop(2)))
-        case '(':
-            pass
-        case ')' | 'c':
-            loop_continue()
-        case '*':
-            st.append(operation(mul, *stack_pop(2)))
-        case '+':
-            st.append(operation(add, *stack_pop(2)))
-        case '-':
-            st.append(operation(sub, *stack_pop(2)))
-        case '/':
-            st.append(operation(lambda a, b: a * (Fraction(1) / b), *stack_pop(2)))
-        case _ if ch.isdigit():
-            st.append(int(ch))
-        case '<':
-            st.append(operation(lambda x, y: int(x < y), *stack_pop(2)))
-        case '=':
-            st.append(operation(lambda x, y: int(x == y), *stack_pop(2)))
-        case '>':
-            st.append(operation(lambda x, y: int(x > y), *stack_pop(2)))
-        case 'C':
-            st.append(operation(ceil, *stack_pop()))
-        case 'D':
-            st.append(operation(lambda x: x.denominator, *stack_pop()))
-        case 'F':
-            st.append(operation(factorial, *stack_pop()))
-        case 'I':
-            st.append(operation(int, *stack_pop()))
-        case 'N':
-            st.append(operation(lambda x: x.numerator, *stack_pop()))
-        case 'R':
-            st.append(operation(round, *stack_pop()))
-        case 'S':
-            st.append(operation(str, *stack_pop()))
-        case 'T':
-            st.append(operation(trunc, *stack_pop()))
-        case '[':
-            if not_(*stack_pop()):
-                if_skip()
-        case ']':
-            pass
-        case '^':
-            st.append(operation(xor, *stack_pop(2)))
-        case '`':
-            st.append(operation(chr, *stack_pop()))
-        case 'b':
-            loop_break()
-        case 'd':
-            st.extend(chain.from_iterable(tee(stack_pop())))
-        case 'g':
-            x = ~next(stack_pop())
-            try:
-                st[-1], st[x] = st[x], st[-1]
-            except:
-                raise_exception('Invalid Index')
-        case 'i':
-            c = stdin.read(1)
-            st.append(ord(c) if c else -1)
-        case 'o':
-            operation(print, *stack_pop(), end='')
-        case 'p':
-            next(stack_pop())
-        case 's':
-            st.extend(tuple(stack_pop(2)))
-        case 't':
-            exit(0)
-        case 'x':
-            try:
-                st.append(st[~next(stack_pop())])
-            except:
-                raise_exception('Invalid Index')
-        case '|':
-            st.append(operation(or_, *stack_pop(2)))
-        case '~':
-            st.append(operation(inv, *stack_pop()))
-        case _ if ch.isspace():
-            pass
-        case _:
-            raise_exception(f'Invalid command {repr(ch)}')
-    i += 1
-    if args.debug and not ch.isspace():
-        print(st)
+    while i < len(code):
+        ch = code[i]
+        
+        if not ch.isspace():  # Only show relevant commands and their stack states
+            clear_screen()
+            print(f'Command: {ch}')
+            print(f'Stack: {st}')
+            input("Press Enter to continue...")  # Wait for user input before proceeding
+        
+        match ch:
+            case '!':
+                st.append(operation(lambda x: int(not_(x)), *stack_pop()))
+            case '%':
+                st.append(operation(mod, *stack_pop(2)))
+            case '&':
+                st.append(operation(and_, *stack_pop(2)))
+            case '(':
+                pass
+            case ')' | 'c':
+                loop_continue()
+            case '*':
+                st.append(operation(mul, *stack_pop(2)))
+            case '+':
+                st.append(operation(add, *stack_pop(2)))
+            case '-':
+                st.append(operation(sub, *stack_pop(2)))
+            case '/':
+                st.append(operation(lambda a, b: a * (Fraction(1) / b), *stack_pop(2)))
+            case _ if ch.isdigit():
+                st.append(int(ch))
+            case '<':
+                st.append(operation(lambda x, y: int(x < y), *stack_pop(2)))
+            case '=':
+                st.append(operation(lambda x, y: int(x == y), *stack_pop(2)))
+            case '>':
+                st.append(operation(lambda x, y: int(x > y), *stack_pop(2)))
+            case 'C':
+                st.append(operation(ceil, *stack_pop()))
+            case 'D':
+                st.append(operation(lambda x: x.denominator, *stack_pop()))
+            case 'F':
+                st.append(operation(factorial, *stack_pop()))
+            case 'I':
+                st.append(operation(int, *stack_pop()))
+            case 'N':
+                st.append(operation(lambda x: x.numerator, *stack_pop()))
+            case 'R':
+                st.append(operation(round, *stack_pop()))
+            case 'S':
+                st.append(operation(str, *stack_pop()))
+            case 'T':
+                st.append(operation(trunc, *stack_pop()))
+            case '[':
+                if not_(*stack_pop()):
+                    if_skip()
+            case ']':
+                pass
+            case '^':
+                st.append(operation(xor, *stack_pop(2)))
+            case '`':
+                st.append(operation(chr, *stack_pop()))
+            case 'b':
+                loop_break()
+            case 'd':
+                st.extend(chain.from_iterable(tee(stack_pop())))
+            case 'g':
+                x = ~next(stack_pop())
+                try:
+                    st[-1], st[x] = st[x], st[-1]
+                except:
+                    raise_exception('Invalid Index')
+            case 'i':
+                c = stdin.read(1)
+                st.append(ord(c) if c else -1)
+            case 'o':
+                operation(print, *stack_pop(), end='')
+            case 'p':
+                next(stack_pop())
+            case 's':
+                st.extend(tuple(stack_pop(2)))
+            case 't':
+                exit(0)
+            case 'x':
+                try:
+                    st.append(st[~next(stack_pop())])
+                except:
+                    raise_exception('Invalid Index')
+            case '|':
+                st.append(operation(or_, *stack_pop(2)))
+            case '~':
+                st.append(operation(inv, *stack_pop()))
+            case _ if ch.isspace():
+                pass
+            case _:
+                raise_exception(f'Invalid command {repr(ch)}')
+        
+        i += 1
+
+        if args.debug and not ch.isspace():
+            print(st)
